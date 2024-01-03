9cf4b9106f0da926553f7f8ad97c93b8bbb76b35
Aanas Aamer <anasamer056@gmail.com>
2024-01-03 18:19:17 +0200

    Implement add-book feature

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const closeBtn = document.querySelector("#cancel-btn");
 const backDrop = window.getComputedStyle(addBookDialog, "::backdrop");
 const saveBtn = document.querySelector("#save-btn");
 const dialogWrapper = document.querySelector("#wrapper");
+const bookGrid = document.querySelector("#book-grid");
 
 const books = [];
 
@@ -30,7 +31,8 @@ addBookForm.addEventListener("submit", ()=>{
         data.get("read")
     )
     books.push(book);
-    console.dir(books)
+    console.dir(books);
+    updateBookGrid();
     addBookForm.reset();
 })
 
@@ -42,7 +44,67 @@ dialogWrapper.addEventListener("click", (e)=>{
     e.stopPropagation();
 })
 
+function clearBookGrid() {
+    bookGrid.innerHTML = "";
+}
 
+function updateBookGrid() {
+    // Clear grid to start on a clean slate
+    clearBookGrid();
+
+    for (const book of books) {
+        // Create the card
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        // Create title, author, pages
+        const title = document.createElement("h3");
+        title.textContent = book.title;
+        
+        const author = document.createElement("h4");
+        author.textContent = book.author;
+        
+        const pages = document.createElement("h4");
+        pages.textContent = book.pages;
+
+        // Create read status
+        let isRead; 
+        let spanClass;
+        if (book.readStatus) {
+            isRead = "read";
+            spanClass = "read";
+        } else {
+            isRead = "not read";
+            spanClass = "not-read"
+        }
+
+        const readStatus = document.createElement("div");
+        readStatus.classList.add("flex-space-bet");
+
+        const p = document.createElement("p");
+        p.innerHTML = `Status: <span class="${spanClass}">${isRead}</span>`;
+        
+
+        const changeBtn = document.createElement("button");
+        changeBtn.classList.add("btn", "btn-text");
+        changeBtn.textContent = "Change";
+
+        readStatus.appendChild(p);
+        readStatus.appendChild(changeBtn);
+
+        // Create remove button
+        const removeBtn = document.createElement("button");
+        removeBtn.classList.add("btn", "btn-red");
+        removeBtn.textContent = "Remove";        
+        
+        // Append all children 
+        const elements = [title, author, pages, readStatus, removeBtn];
+        for (const element of elements) {
+            card.appendChild(element);
+        }
+        bookGrid.appendChild(card);
+    }
+}
 
 
 function Book(title, author, pages, readStatus) {
