8968efb74c53aa212de4b61623b8fd94098888c7
Aanas Aamer <anasamer056@gmail.com>
2024-01-05 01:05:00 +0200

    Create basic module structure

---
+++ README.md
@@ -0,0 +1,7 @@
+# Personal Library 
+
+### Live Preview
+https://anasamer056.github.io/library/
+
+### Notable Features 
+TODO
--- script.js
+++ script.js
@@ -1,118 +1,109 @@
+const databse = (function(){
+    const books = [];
+    getBooks = ()=> books;
+    addBook = (book)=> books.push(book);
+    removeBook = (bookIndex)=> books.splice(bookIndex, 1);  
+    return {books, getBooks, addBook, removeBook};
+})()
 
-const addBtn = document.querySelector("#add-btn");
-const addBookDialog = document.querySelector("#add-book");
-const addBookForm = document.querySelector("#add-book-form")
-const closeBtn = document.querySelector("#cancel-btn");
-const backDrop = window.getComputedStyle(addBookDialog, "::backdrop");
-const saveBtn = document.querySelector("#save-btn");
-const dialogWrapper = document.querySelector("#wrapper");
-const bookGrid = document.querySelector("#book-grid");
-
-
-const books = [];
-
-console.dir(addBookForm);
-
-addBtn.addEventListener("click", ()=>{
-    addBookDialog.showModal();
-})
-
-closeBtn.addEventListener("click", (e)=>{
-    e.preventDefault();
-    addBookDialog.close("cancel");
-    addBookForm.reset();
-})
-
-addBookForm.addEventListener("submit", ()=>{
-    const data = new FormData(addBookForm);
-    const book = new Book(
-        data.get("title"),
-        data.get("author"),
-        data.get("pages"),
-        data.get("read")
-    )
-    books.push(book);
-    console.dir(books);
-    updateBookGrid();
-    addBookForm.reset();
-})
-
-bookGrid.addEventListener("click" , (e)=>{
-    if (e.target.classList.contains("change-btn")){
-        const bookIndex = e.target.parentNode.parentNode.dataset.index;
-        books[bookIndex].changeReadStatus();
-        updateBookGrid();
-    } else if (e.target.classList.contains("remove-btn")) {
-        const bookIndex = e.target.parentNode.dataset.index;
-        books.splice(bookIndex, 1);
-        updateBookGrid();
+const bookgrid = (function(){
+    const bookGrid = document.querySelector("#book-grid");
+    
+    function clear() {
+        bookGrid.innerHTML = "";
     }
+    
+    function update(books) {
+        // Clear grid to start on a clean slate
+        clear();
+    
+        books.forEach((book, index)=> {
+            // Create the card
+            const card = document.createElement("div");
+            card.classList.add("card");
+            card.setAttribute("data-index", index)
+    
+            // Create title, author, pages
+            const title = document.createElement("h3");
+            title.textContent = book.title;
+            
+            const author = document.createElement("h4");
+            author.textContent = "By: " + book.author;
+            
+            const pages = document.createElement("h4");
+            pages.textContent = "Pages: " + book.pages;
+    
+            // Create read status
+            const spanClass = book.readStatus ? "read" : "not-read"
+          
+            const readStatus = document.createElement("div");
+            readStatus.classList.add("flex-space-bet");
+    
+            const p = document.createElement("p");
+            p.innerHTML = `Status: <span class="${spanClass}"></span>`;
+            
+    
+            const changeBtn = document.createElement("button");
+            changeBtn.classList.add("btn", "btn-text", "change-btn");
+            changeBtn.textContent = "Change";
+    
+            readStatus.appendChild(p);
+            readStatus.appendChild(changeBtn);
+    
+            // Create remove button
+            const removeBtn = document.createElement("button");
+            removeBtn.classList.add("btn", "btn-red", "remove-btn");
+            removeBtn.textContent = "Remove";        
+            
+            // Append all children 
+            const elements = [title, author, pages, readStatus, removeBtn];
+            for (const element of elements) {
+                card.appendChild(element);
+            }
+            bookGrid.appendChild(card);
+        })
+    }
+    return {clear, update};
+})()
 
-});
-
-
-// Close dialog when user clicks outside it. 
-addBookDialog.addEventListener("click", (e)=> {
-    addBookDialog.close("cancel");
-});
-dialogWrapper.addEventListener("click", (e)=>{
-    e.stopPropagation();
-})
-
-function clearBookGrid() {
-    bookGrid.innerHTML = "";
-}
-
-function updateBookGrid() {
-    // Clear grid to start on a clean slate
-    clearBookGrid();
-
-    books.forEach((book, index)=> {
-        // Create the card
-        const card = document.createElement("div");
-        card.classList.add("card");
-        card.setAttribute("data-index", index)
-
-        // Create title, author, pages
-        const title = document.createElement("h3");
-        title.textContent = book.title;
-        
-        const author = document.createElement("h4");
-        author.textContent = "By: " + book.author;
-        
-        const pages = document.createElement("h4");
-        pages.textContent = "Pages: " + book.pages;
-
-        // Create read status
-        const spanClass = book.readStatus ? "read" : "not-read"
-      
-        const readStatus = document.createElement("div");
-        readStatus.classList.add("flex-space-bet");
-
-        const p = document.createElement("p");
-        p.innerHTML = `Status: <span class="${spanClass}"></span>`;
-        
-
-        const changeBtn = document.createElement("button");
-        changeBtn.classList.add("btn", "btn-text", "change-btn");
-        changeBtn.textContent = "Change";
-
-        readStatus.appendChild(p);
-        readStatus.appendChild(changeBtn);
-
-        // Create remove button
-        const removeBtn = document.createElement("button");
-        removeBtn.classList.add("btn", "btn-red", "remove-btn");
-        removeBtn.textContent = "Remove";        
-        
-        // Append all children 
-        const elements = [title, author, pages, readStatus, removeBtn];
-        for (const element of elements) {
-            card.appendChild(element);
-        }
-        bookGrid.appendChild(card);
+const displayController = (function(){
+    const addBtn = document.querySelector("#add-btn");
+    const closeBtn = document.querySelector("#cancel-btn");
+    const addBookDialog = document.querySelector("#add-book");
+    const addBookForm = document.querySelector("#add-book-form")
+    const dialogWrapper = document.querySelector("#wrapper");
+    
+    addBtn.addEventListener("click", ()=>{
+        addBookDialog.showModal();
     })
-}
+    closeBtn.addEventListener("click", (e)=>{
+        e.preventDefault();
+        addBookDialog.close("cancel");
+        addBookForm.reset();
+    })
+
+    addBookForm.addEventListener("submit", ()=>{
+        const data = new FormData(addBookForm);
+        const book = new Book(
+            data.get("title"),
+            data.get("author"),
+            data.get("pages"),
+            data.get("read")
+        )
+        databse.addBook(book);
+        bookgrid.update(databse.getBooks());
+        addBookForm.reset();
+    })
+    
+    
+    // Close dialog when user clicks outside it. 
+    addBookDialog.addEventListener("click", (e)=> {
+        addBookDialog.close("cancel");
+    });
+    dialogWrapper.addEventListener("click", (e)=>{
+        e.stopPropagation();
+    })  
+})()
 
 
 function Book(title, author, pages, readStatus) {
